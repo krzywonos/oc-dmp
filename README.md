b52a97d5aec0ec3df70014504412c0e58e43ffb0
Hubert Krzywonos <krzywonos.h@gmail.com>
2026-02-02 15:48:47 +0100

    changes as discussed during the in-person meeting

--- README.md
+++ README.md
@@ -1,9 +1,149 @@
-Running the script using CLI:
+# General information
+OC-DMP (Open Citations - Data Management Plan) is a generator of University of Bologna-compliant Data Management Plans for the OpenCitations research group.
+The script was made as a project for the Open Science class at University of Bologna, Academic Year 2024/25.
+
+### Running the script using CLI
 
 python script.py --data data
 
 The --data flag is used to specify a subfolder for the input data, without it the script will look for it in the same folder it is located in.
 
+### Protocol
+
 Protocol available at:
 
 https://www.protocols.io/view/open-science-24-25-hubert-v2-hhqab35sf
+
+# Filling the input data
+
+## Example files
+A folder *example_input* with example input files has been made available.
+
+### Markdown files
+
+Markdown files are loaded into the final document using the markdown package.
+For more information on how Markdown works: https://daringfireball.net/projects/markdown/syntax
+For the list of differences between Markdown and markdown package: https://python-markdown.github.io/#differences
+
+### general_data.xlsx
+The script creates <\h3> headers for the final document based on the cells in column A.
+
+The consequent cells in the same row serve as values for <\p> paragraphs that are entered below the corresponding headers.
+
+An exception is the row starting with the cell "Title of DMP". There, the first cell is used to fill the title of the final HTML document.
+
+Fields accept natural language as input, except if specified otherwise. The following list contain the fields and their meanings:
+*Title of DMP* - Title of the Data Management Plan
+*Description* - Brief description of the Data Management Plan
+*Language* - The language of the Data Management Plan
+*Contact* - Contact information of the person responsible for the Data Management Plan
+*Researchers* - Names of people, that have produced, processed and/or analysed the research output described in the Data Management Plan
+*Organisations* - Names of organisations contributing to the creation and revision of the Data Management Plan
+*Funding organisations* - Names of organisation providing funding towards the research output described in the Data Management Plan
+*Grants* - Information about grants given towards the research output provided in the Data Management Plan
+*Project* - Information about the project the research output is a part of
+*Access Rights* - Information regarding the access of the Data Management Plan on Zenodo
+*Licence* - Information about the licence of the Data Management Plan.
+
+### data_table.xlsx
+The main data sheet is currently locked with the password "123" to avoid inputting the data in the columns that represent headings, which serve only for organisational reasons.
+
+After unlocking the main data sheet, text can be used in the columns under these headings to provide additional information, if necessary.
+
+Headers for the subsections of the final HTML are taken from the cells in the first row of the spreadsheet. The numbering is removed for readability reasons.
+
+Each row corresponds to a research output. The first cell is the name of the research output. The corresponding cells in the same row represent paragraphs that will be used as paragraphs under the headings specified in the first row.
+
+If a cell displays a value *N/A*, the subsection is entirely omitted from the final document.
+
+There are cells where the user may choose to input *N/A* manually, this will omit the subsection from being included in the final document.
+
+Cells may contain selectors for Yes/No values or Dataset/Software/Object values. The user may choose to manually input a different value, but it will result in conditional colour formatting of the cells to behave improperly.
+
+Cells may contain formulas to automatically input *N/A* where applicable (e.g. if a research output is selected as a Dataset in subsection 1.3.2., then the cells in the subsection 2.3., 2.4., 2.5. and 2.6. will be automatically set to *N/A*). The user should overwrite the formulas in the applicable rows only, but may choose to overwrite them in other cells, where it will result in conditional colour formatting of the cells to behave improperly.
+
+The following list contain the fields, their meaning, their accepted values (if applicable), if they are selector or formula cells; and if they are locked for the purposes of the final document generation:
+
+*1.* Your research output - locked.
+*1.1.* Contact details and responsibilities - locked.
+*1.1.1.* Contact person(s) - Contact information of the person or people responsible for the Data Management Plan of the research output. Accepts a list of people (Surname, Name \[pid:0123\]) separated by semicolons.
+*1.1.2.* Is the contact person also the research output creator - selector cell - Yes if the contact person is one of the research output creators, No otherwise.
+*1.1.3.* Research output creator(s) and contributor(s) - List of all people who have contributed towards the creation of the research output. Accepts a list of people (Surname, Name \[pid:0123\]) separated by semicolons.
+*1.1.4.* Institution(s) involved - List of all institutions involved in the creation of the research output. Accepts a list of institutions (Institution Name \[pid:0123\]) separated by semicolons.
+*1.2.* Resources for data management - locked.
+*1.2.1.* What resources will be dedicated to data management and ensuring that data will be FAIR? - Information regarding the resources dedicated to management and FAIR-ensurement of data.
+*1.3.* Identifying the research output - locked. 
+*1.3.1.* Work package and task number - Work package and task number of the research output.
+*1.3.2.* Which type of research output are you describing - Selector cell of dataset/software/object. Custom input is accepted, but the cells in the columns of section 2 have to be formatted manually.
+*2.* Summary - locked.
+*2.1.* Identification of the dataset - locked.
+*2.1.1.* Are you re-using an already existing dataset? - Selector cell - Yes if the dataset has existed before, No otherwise.
+*2.1.2.* Re-using an existing dataset: why and how - Formula cell - Explanation for and description of the re-use of an existing dataset.
+*2.1.3.* Creating a new dataset - Selector cell - Yes if the dataset is created as part of the research, No otherwise.
+*2.2.* Dataset characteristics - locked.
+*2.2.1.* Dataset name - Formula cell - Name of the dataset provided as part of the research output.
+*2.2.2.* Data types - Formula cell - Types of data provided as part of the research output.
+*2.2.3.* Data formats - Formula cell - Formats of data provided as part of the research output.
+*2.2.4.* Size of data - Formula cell - Compressed and uncompressed sizes on a storage drive of data provided as part of the research output.
+*2.2.5.* How is the data generated/collected - Formula cell - Information on the generation/collection of data provided as part of the research output.
+*2.2.6.* Why is the data generated/collected - Formula cell - Rationale for the generation/collection of data provided as part of the research output.
+*2.2.7.* Dataset description - Formula cell - Other information relating to the data provided as the research output that has not been described in the previous subsection.
+*2.3.* Identification of software - locked.
+*2.3.1.* Are you re-using already existing software - Selector cell - Yes if the software has existed before, No otherwise.
+*2.3.2.* Re-using existing software: why and how - Formula cell - Explanation for and description of the re-use of an existing software.
+*2.3.3.* Creating new software - Selector cell - Yes if the software is created as part of the research, No otherwise.
+*2.4.* Software characteristics - locked.
+*2.4.1.* Software name - Formula cell - Name of the software provided as part of the research output.
+*2.4.2.* Type of software - Formula cell - Type of software provided as part of the research output.
+*2.4.3.* Software formats - Formula cell - Formats of software provided as part of the research output.
+*2.4.4.* Size of software - Formula cell - Compressed and uncompressed sizes on a storage drive of software provided as part of the research output.
+*2.4.5.* How is software created/used - Formula cell - Information on the creation/use of software provided as part of the research output.
+*2.4.6.* Why is software created/used - Formula cell - Rationale for the creation/use of software provided as part of the research output.
+*2.4.7.* Software description - Formula cell - Other information relating to the software provided as the research output that has not been described in the previous subsection.
+*2.5.* Identification of a non-digital object - locked.
+*2.5.1.* Are you re-using an already existing object? - Selector cell - Yes if the object has existed before, No otherwise.
+*2.5.2.* Re-using an existing object: why and how - Formula cell - Explanation for and the description of the re-use of an existing object.
+*2.5.3.* Creating a new object - Selector cell - Yes if the object is created as part of the research, No otherwise.
+*2.6.* Non-digital object characteristics - locked.
+*2.6.1.* Object name - Formula cell - Name of the object provided as part of the research output.
+*2.6.2.* Type of object - Formula cell - Type of the object provided as part of the research output.
+*2.6.3.* How is the object created/used - Formula cell - Information on the creation/use of object provided as part of the research output.
+*2.6.4.* Why is the object created/used - Formula cell - Rationale for the creation/use of object provided as part of the research output.
+*3.* Ethics - locked.
+*3.1.* Personal data - locked.
+*3.1.1.* Are you handling personal data? - Selector cell - Yes if the research output has made use of personal data, No otherwise.
+*3.1.2.* Solutions adopted for handling personal data - Formula cell - Description of solutions used for handling and anonymising personal information used to create the research output.
+*3.2.* Legal aspects - locked.
+*3.2.1.* Are there any legal issues associated to your output (e.g. IPR or valorisation)? - Selector cell - Yes if there are legal issues associated to the research output, No otherwise.
+*3.2.2.* Legal issues: description and solutions - Formula cell - Description of the legal issues associated to the research output and solutions for their accommodation and mitigation.
+*3.3.* Other ethical aspects - locked.
+*3.3.1.* Are there other ethical issues associated to your output? - Selector cell - Yes if there are other ethical issues associated to the research output, No otherwise.
+*3.3.2.* Ethical issues: description and solutions - Formula cell - Description of the ethical issues associated to the research output and solutions for their accommodation and mitigation.
+*4.* Making research FAIR - locked.
+*4.1.* Long-term preservation - locked.
+*4.1.1.* Selecting what to preserve - Rationale on the selection of data/code/other things selected to be preserved as part of the research output.
+*4.1.2.* Chosen repository/ies - Repositories for the files/objects made as the research output.
+*4.1.3.* Further information on long-term preservation strategy - Other information on long-term preservation strategy of the research output.
+*4.2.* Findability - locked.
+*4.2.1.* Persistent identifier(s) - Persistent identifiers of the dataset/software/other object made as part of the research output.
+*4.2.2.* Metadata - Information on the metadata of the dataset/software/other object made as part of the research output.
+*4.2.3.* Keywords - Keywords used to facilitate searching for the dataset/software/other object made as part of the research output. Accepts a list of keywords separated by semicolons.
+*4.3.* Accessibility - locked.
+*4.3.1.* Is the research output openly accessible? - Selector cell - Yes if the research output has been made openly accessible, No otherwise.
+*4.3.2.* Controlled access - Selector and formula cell - Yes if the access to the research output is controlled, No otherwise.
+*4.3.3.* Accessing the research output - Information on how to access the research output.
+*4.3.4.* Is metadata openly accessible? - Selector cell - Yes if metadata of the research output is openly accessible, No otherwise.
+*4.3.5.* Accessing metadata - Information on how to access the research output.
+*4.4.* Interoperability - locked.
+*4.4.1.* Methodologies - Methodologies used to produce the research output.
+*4.4.2.* Vocabularies, taxonomies and other standards - Vocabularies, taxonomies and other standards used to produce the research output.
+*4.5.* Reusability - locked.
+*4.5.1.* Licencing - Licence of the research output.
+*4.5.2.* Documentation - Link to the documentation of the research output.
+*5.* Quality and security - locked.
+*5.1.* Quality of the research output - locked.
+*5.1.1.* Measures implemented to ensure quality - Information on quality assurance of the research output.
+*5.2.* Security measures - locked.
+*5.2.1.* Storage - Information on the primary storage of the research output.
+*5.2.2.* Backup - Information on the secondary storage of the research output
+*5.2.3.* Additional security - Information on the additional security of the research output.
--- script.py
+++ script.py
@@ -1,291 +1,194 @@
 import argparse
-import html
+import os
 import re
-import sys
+from collections import defaultdict
 from pathlib import Path
-
 from openpyxl import load_workbook
-import markdown as md
+import markdown
 
-# Helpers
+# helpers
 
-def read_text_file(path: Path) -> str:
-    return path.read_text(encoding="utf-8");
-
-
-def md_to_html(markdown_text: str) -> str:
-    return md.markdown(
-        markdown_text,
-        extensions=[
-            "extra",
-            "tables",
-            "fenced_code",
-            "sane_lists",
-        ],
-        output_format="html5",
-    );
+def slugify(text):
+    """Create a safe id for HTML anchors."""
+    text = text.lower();
+    text = re.sub(r"[^\w\s-]", "", text); # regex is AI-generated
+    text = re.sub(r"\s+", "-", text.strip()); # regex is AI-generated
+    return text;
 
 
-def cell_to_str(value) -> str:
-    if value is None:
-        return "";
-    return str(value).strip();
+def read_markdown(path):
+    """Convert Markdown file to HTML."""
+    with open(path, "r", encoding="utf-8") as f:
+        return markdown.markdown(f.read());
 
+# general_dmp.xlsx
 
-def heading_level_from_numbering(text: str) -> int | None:
-    """
-    "1. Title"     -> h1
-    "1.1. Title"   -> h2
-    "1.1.1. Title" -> h3
-    """
-    numbering = text.strip();
-    matched_numbering = re.match(r"^\s*(\d+(?:\.\d+)*)\.\s+.+", numbering); # regex is AI-generated
-    if not matched_numbering:
-        return None;
-    segments = matched_numbering.group(1).split(".");
-    return max(1, min(6, len(segments)));
-
-
-TRIPLE_CODE_RE = re.compile(r"^\s*(\d+)\.(\d+)\.(\d+)\.\s*(.*)$"); # regex is AI-generated
-
-
-def parse_triple_code(text: str):
-    """
-    Matches:
-      "1.3.1. Something" or "1.3.1."
-    """
-    numbering = text.strip();
-    matched_numbering = TRIPLE_CODE_RE.match(numbering);
-    if not matched_numbering:
-        return None
-    major = int(matched_numbering.group(1));
-    sub = int(matched_numbering.group(2));
-    item = int(matched_numbering.group(3));
-    rest = (matched_numbering.group(4) or "").strip();
-    code = f"{major}.{sub}.{item}.";
-    return {"code": code, "major": major, "sub": sub, "item": item, "rest": rest};
-
-
-def normalise_type_choice(input_string: str) -> str:
-    normalised_input = (input_string or "").strip().lower();
-    if normalised_input == "dataset":
-        return "dataset";
-    if normalised_input == "software":
-        return "software";
-    if normalised_input == "object":
-        return "object";
-    return "";
-
-
-def should_include_section2(sub: int, choice: str) -> bool:
-    """
-    Dataset  -> include 2.1.* and 2.2.*
-    Software -> include 2.3.* and 2.4.*
-    Object   -> include 2.5.* and 2.6.*
-    Unknown  -> include all 2.*
-    """
-    if not choice:
-        return True;
-    if choice == "dataset":
-        return sub in {1, 2};
-    if choice == "software":
-        return sub in {3, 4};
-    if choice == "object":
-        return sub in {5, 6};
-    return True;
-
-# Parsers
-
-def parse_general_dmp_xlsx(path: Path) -> str:
+def parse_general_dmp(path):
     """
     Parses general information about DMP from general_dmp.xlsx.
-    Headers are made from column A starting with cell A2.
-    Paragraphs under the headers are made from subsequent columns in the same row.
+    Value of the 2st cell in the row starting with "Title of DMP" is translated into the title of the document.
+    Values from column A are translated to <h3> headings.
+    Values from the next columns in the same row are translated into <p> paragraphs.
     """
-    workbook = load_workbook(path, data_only=True);
-    worksheet = workbook.active;
+    wb = load_workbook(path);
+    ws = wb.active;
 
-    parts: list[str] = [];
-    max_row = worksheet.max_row;
-    max_col = worksheet.max_column;
+    title = None;
+    html_parts = [];
 
-    for row in range(2, max_row + 1):
-        title = cell_to_str(worksheet.cell(row=row, column=1).value);
-        if not title:
+    for row in ws.iter_rows(min_row=2, values_only=True):
+        heading = row[0];
+        if not heading:
             continue;
 
-        parts.append(f"<h3>{html.escape(title)}</h3>");
+        if heading == "Title of DMP":
+            title = row[1];
+            continue;
 
-        for col in range(2, max_col + 1):
-            value = cell_to_str(worksheet.cell(row=row, column=col).value);
-            if not value:
-                continue;
-            parts.append(f"<p>{html.escape(value)}</p>");
+        html_parts.append(f"<h3>{heading}</h3>");
 
-    return "\n".join(parts);
+        for cell in row[1:]:
+            if cell:
+                html_parts.append(f"<p>{cell}</p>");
+
+    return title, "\n".join(html_parts);
 
 
-def parse_data_table_xlsx(path: Path) -> str:
+
+# data_table.xlsx
+
+def parse_data_table(path):
     """
     Parses DMP data for the research output from data_table.xlsx.
     Headers are taken from cells in the first row.
     Paragraphs for sections are taken from the cells in the same column.
     Paragraph begins with the name of the research output specified in the same row in column A.
-    Section 1.3.2. is currently hard-coded, as it is acting as a switch statement for which subsection in section 2 to include.
     """
-    workbook = load_workbook(path, data_only=True);
+    wb = load_workbook(path, data_only=True);
+    ws = wb.active;
 
-    worksheet = workbook["Main data sheet"] if "Main data sheet" in workbook.sheetnames else workbook.active;
+    # parse heading definitions from first row
+    heading_defs = {};
+    heading_levels = {};
 
-    max_row = worksheet.max_row;
-    max_col = worksheet.max_column;
-
-    # headers from row 1, columns B..end
-    col_headers: dict[int, str] = {};
-    for col in range(2, max_col + 1):
-        col_headers[col] = cell_to_str(worksheet.cell(row=1, column=col).value);
-
-    # find selector column (header starts with 1.3.2.)
-    selector_col = None;
-    for col, header in col_headers.items():
-        triple = parse_triple_code(header);
-        if triple and triple["code"] == "1.3.2.":
-            selector_col = col;
-            break;
-
-    parts: list[str] = [];
-
-    for col in range(2, max_col + 1):
-        header = col_headers.get(col, "");
-        if not header:
+    for col in range(2, ws.max_column + 1):
+        cell_value = ws.cell(row=1, column=col).value;
+        if not cell_value:
             continue;
 
-        # emit numbered headings (h1/h2/...)
-        lvl = heading_level_from_numbering(header);
-        if lvl is not None:
-            parts.append(f"<h{lvl}>{html.escape(header)}</h{lvl}>");
-
-        # only triple-coded columns produce row-based paragraphs
-        triple = parse_triple_code(header);
-        if not triple:
+        matches = re.match(r"((\d+\.)+)\s*(.*)", cell_value); # regex is AI-generated
+        if not matches:
             continue;
 
-        code = triple["code"];
-        major = triple["major"];
-        sub = triple["sub"];
+        numbering = matches.group(1);
+        text = matches.group(3);
+        level = numbering.count(".");
 
-        paras: list[str] = [];
-        for r in range(2, max_row + 1):
-            row_label = cell_to_str(worksheet.cell(row=r, column=1).value)
-            if not row_label:
+        heading_defs[col] = text;
+        heading_levels[col] = level;
+
+    html_parts = [];
+
+    for row in range(2, ws.max_row + 1):
+        output_name = ws.cell(row=row, column=1).value;
+        if not output_name:
+            continue;
+
+        section_id = slugify(output_name);
+        html_parts.append(f'<h1 id="{section_id}">{output_name}</h1>');
+
+        collected = defaultdict(list);
+
+        for col, heading in heading_defs.items():
+            value = ws.cell(row=row, column=col).value;
+            level = heading_levels[col];
+
+            if level == 1:
+                collected["h1"].append((heading, value));
+            elif level == 2:
+                collected["h2"].append((heading, value));
+            elif level == 3:
+                collected["h3"].append((heading, value));
+
+        # navigation list (links to h1)
+        nav_items = [];
+
+        for heading, _ in collected["h1"]:
+            hid = slugify(f"{output_name}-{heading}");
+            nav_items.append(f'<li><a href="#{hid}">{heading}</a></li>');
+
+        if nav_items:
+            html_parts.append("<ul>");
+            html_parts.extend(nav_items);
+            html_parts.append("</ul>");
+
+        # Render sections
+        for col, heading in heading_defs.items():
+            value = ws.cell(row=row, column=col).value;
+            level = heading_levels[col];
+
+            if level == 3 and value == "N/A":
                 continue;
 
-            val = cell_to_str(worksheet.cell(row=r, column=col).value);
-            if not val:
-                continue;
-
-            # apply special filtering to section 2 columns only
-            if major == 2 and selector_col is not None:
-                choice = normalise_type_choice(cell_to_str(worksheet.cell(row=r, column=selector_col).value));
-                if not should_include_section2(sub=sub, choice=choice):
+            if level == 2:
+                has_children = any(
+                    v != "N/A"
+                    for h, v in collected["h3"]
+                    if h.startswith(heading)
+                );
+                if not has_children:
                     continue;
 
-            paras.append(f"<p>{html.escape(row_label)} - {html.escape(val)}</p>");
+            hid = slugify(f"{output_name}-{heading}");
+            html_parts.append(f'<h{level} id="{hid}">{heading}</h{level}>');
 
-        # only emit the <h3>code</h3> if there is something under it
-        if paras:
-            parts.extend(paras);
+            if level == 3 and value:
+                html_parts.append(f"<p>{value}</p>");
 
-    return "\n".join(parts);
-
-
-# assembly
-
-def build_full_html(data_dir: Path) -> str:
-    """
-    
-    """
-    required = [
-        "introduction.md",
-        "general_dmp.xlsx",
-        "data.md",
-        "data_table.xlsx",
-        "conclusion.md",
-        "acknowledgements.md",
-        "references.md",
-    ];
-
-    missing = [name for name in required if not (data_dir / name).exists()];
-    if missing:
-        raise FileNotFoundError(
-            "Missing required input files in data directory:\n  - "
-            + "\n  - ".join(missing)
-        );
-
-    parts: list[str] = [];
-
-    parts.append(md_to_html(read_text_file(data_dir / "introduction.md")));
-    parts.append(parse_general_dmp_xlsx(data_dir / "general_dmp.xlsx"));
-    parts.append(md_to_html(read_text_file(data_dir / "data.md")));
-    parts.append(parse_data_table_xlsx(data_dir / "data_table.xlsx"));
-    parts.append(md_to_html(read_text_file(data_dir / "conclusion.md")));
-    parts.append(md_to_html(read_text_file(data_dir / "acknowledgements.md")));
-    parts.append(md_to_html(read_text_file(data_dir / "references.md")));
-
-    body = "\n\n".join(parts);
-
-    return f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <title>DMP</title>
-  <style>
-    body {{
-      font-family: Arial, Helvetica, sans-serif;
-      line-height: 1.35;
-      margin: 32px;
-    }}
-    h1, h2, h3, h4, h5, h6 {{
-      margin-top: 1.1em;
-      margin-bottom: 0.4em;
-    }}
-    p {{
-      margin: 0.2em 0 0.8em 0;
-      white-space: pre-wrap; /* preserves Excel line breaks */
-    }}
-  </style>
-</head>
-<body>
-{body}
-</body>
-</html>
-""";
+    return "\n".join(html_parts);
 
 
-def main() -> int:
-    parser = argparse.ArgumentParser(description="Generate dmp.html from Markdown + Excel inputs.");
-    parser.add_argument(
-        "--data",
-        dest="data_dir",
-        default=None,
-        help="Path to folder containing input files. If omitted, uses the script's directory.",
-    );
+
+# main
+
+
+def main():
+    parser = argparse.ArgumentParser();
+    parser.add_argument("--data", help="Data subfolder", default=".");
     args = parser.parse_args();
 
-    script_dir = Path(__file__).resolve().parent;
-    data_dir = Path(args.data_dir).resolve() if args.data_dir else script_dir;
+    base = args.data;
 
-    try:
-        html_out = build_full_html(data_dir);
-    except Exception as e:
-        print(f"[ERROR] {e}", file=sys.stderr);
-        return 1;
+    title, general_html = parse_general_dmp(
+        os.path.join(base, "general_dmp.xlsx")
+    );
 
-    out_path = script_dir / "dmp.html";
-    out_path.write_text(html_out, encoding="utf-8");
-    print(f"Written: {out_path}");
-    return 0;
+    html = [
+        "<!DOCTYPE html>",
+        "<html>",
+        "<head>",
+        '<meta charset="utf-8">',
+        f"<title>{title or 'DMP'}</title>",
+        '<link rel="stylesheet" href="style.css">',
+        "</head>",
+        "<body>",
+    ];
+
+    html.append(general_html);
+
+    for md in ["introduction.md", "data.md"]:
+        html.append(read_markdown(os.path.join(base, md)));
+
+    html.append(parse_data_table(os.path.join(base, "data_table.xlsx")));
+
+    for md in ["conclusion.md", "acknowledgements.md", "references.md"]:
+        html.append(read_markdown(os.path.join(base, md)));
+
+    html.extend(["</body>", "</html>"]);
+
+    with open("output/dmp.html", "w", encoding="utf-8") as f:
+        f.write("\n".join(html));
 
 
 if __name__ == "__main__":
-    raise SystemExit(main());
+    main();
